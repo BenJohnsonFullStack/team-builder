d14a0a0358774136f45d94a00ed6124016a37266
Ben Johnson <monk.sound.design@gmail.com>
2023-06-02 13:16:22 -0400

    form functions, props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
+import React from 'react';
+import { useState } from 'react';
 
 import './App.css';
 import Form from './Form'
 
 function App() {
+    const initialFormValues = {
+        name: "",
+        email: "",
+        role: "",
+    }
+
+    const [teamMembers, setTeamMembers] = useState([])
+    const [formValues, setFormValues] = useState(initialFormValues)
+    const [formError, setFormError] = useState("")
+
+    const updateForm = (inputName, inputValue) => {
+        setFormValues({ ...formValues, [inputName]: inputValue })
+    }
+
+    const submitForm = () => {
+        const newTeamMember = {
+            name: formValues.name.trim(),
+            email: formValues.email.trim(),
+            role: formValues.role
+          }
+
+        if (!teamMembers.name || !teamMembers.email || !teamMembers.role) {
+            setFormError("All fields are required")
+        }
+
+
+    }
     
     return(
-        <Form />
+        <div className="site-wrapper">
+            <h1>Welcome to the Team!</h1>
+            { formError &&  <h2 className="error">{formError}</h2> }
+            <Form 
+                values={formValues}
+                update={updateForm}
+                submit={submitForm}
+            />
+        </div>
     )
 }
 
--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { useState } from 'react'
 
-export default function Form() {
-    const [formValues, setFormValues] = useState({
-        name: "",
-        email: "",
-        role: "",
-    })
+export default function Form(props) {
+    const { values, update, submit } = props
+
+    
+
+    const onChange = evt => {
+        const { name } = evt.target
+        const { value } = evt.target
+        update(name, value)
+    }
+
+    const onSubmit = (evt) => {
+        evt.preventDefault()
+        submit()
+    }
 
     return (
         <div>
@@ -16,8 +25,8 @@ export default function Form() {
                         type="text"
                         name="name"
                         placeholder="Name"
-                        value={formValues.name}
-                        // onChange={change}
+                        value={values.name}
+                        onChange={onChange}
                     />
                 </label>
                 <label> Name:
@@ -25,19 +34,22 @@ export default function Form() {
                         type="text"
                         name="email"
                         placeholder="Email"
-                        value={formValues.email}
-                        // onChange={change}
+                        value={values.email}
+                        onChange={onChange}
                     />
                 </label>
                 <label htmlFor="role-select">Role:
-                    <select name="role" id="role-select">
+                    <select value={values.role} name="role" id="role-select" onChange={onChange}>
                         <option value="">----Select Your Role----</option>
                         <option value="ui-designer">UI Designer</option>
                         <option value="ux-engineer">UX Engineer</option>
                         <option value="backend-dev">Back-End Developer</option>
                         <option value="frontend-dev">Front-End Developer</option>
                     </select>
-
+                <input
+                    type="submit"
+                    value="Submit"
+                />
                 </label>
             </form>
         </div>
